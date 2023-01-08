7fa9d2d6718ff412250777ea526e040221e9d859
franknhat <franknhat@yahoo.com>
2023-01-08 12:54:20 -0600

    modified shell files

--- .config/i3/i3lock.sh
+++ .config/i3/i3lock.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #scuffed since for my dual monitor setup the "smaller" monitor has like a white bar under it from the blur. Smaller as in my larger external display
-mkdir temp
-flameshot full -p temp/img.png
-convert temp/img.png -blur 0x8 temp/blur.png
-i3lock -i temp/blur.png
-rm -r temp
+mkdir /tmp/temp
+flameshot full -p /tmp/temp/img.png
+convert /tmp/temp/img.png -blur 0x8 /tmp/temp/blur.png
+i3lock -i /tmp/temp/blur.png
+rm -r /tmp/temp
--- diff.sh
+++ diff.sh
@@ -1,13 +1,76 @@
-source ./functions.sh
+#!/bin/bashi
 
-function printDiff(){
-    findDiff differences
+function main(){
+    differences=()
+    
+    function findDiff(){
 
-    for diff in "${differences[@]}"; do
-        echo "in files ${diff}" | lolcat
-        eval "diff ${diff}"
-        echo ''
-    done
+        if ! cmp .bashrc ~/.bashrc &> /dev/null; then
+            differences+=(".bashrc")
+        fi
+
+        for FILE in .config/*; do
+            if ! diff $FILE ~/$FILE &> /dev/null; then
+                differences+=("$FILE")
+            fi
+        done
+        
+    }
+
+
+    function printDiff(){
+        
+        if [ ! "${#differences[@]}" -gt 0 ]; then
+            echo "No differences found!"
+            exit 0
+        fi
+
+        for diff in "${differences[@]}"; do
+            echo "in files ${diff}" | lolcat
+            eval "diff -y ~/${diff} ${diff}"
+            echo ''
+        done
+
+    }
+
+
+    function copyToRepo(){
+
+        for diff in "${differences[@]}"; do
+            if [ -d "${diff}" ]; then
+                eval "cp -r ~/${diff} ${diff}/../." #folder
+            else
+                eval "cp ~/${diff} ${diff}" #file
+            fi
+        done
+        
+    }
+
+
+    findDiff
+    printDiff 
+
+    if [ "${#differences[@]}" -gt 0 ]; then
+         
+        while true
+        do
+            read -r -p "Would you like to copy over all of your different config files to your Repo? (Y/n): " input
+            
+            case $input in
+                [Yy][Ee][Ss] | [yY] | "")
+                    copyToRepo
+                    break
+                    ;;
+                [Nn][Oo] | [Nn])
+                    break
+                    ;;
+                *)
+                    echo "invalid input...."
+                    ;;
+            esac
+        done
+    fi
+
 }
 
-printDiff
+main
